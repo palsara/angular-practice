68001936a20c24ad7727fe3fc466d5cf3651f426
Pál Sára <pal.sara.ajandek@gmail.com>
2019-08-01 09:22:16 +0200

    data-binding ready

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  title: string = "angular-practice-001";
+  names: string[] = [
+    'Pisti',
+    'Sanyi',
+    'Béla',
+    'Laci',
+    'Zsófi',
+    'Eszter',
+  ];
+  name: string;
+  disabled: boolean = false;
+  getName() {
+    return 'Bond, James Bond';
+  }
+  constructor() {
+    this.name = this.names[0];
+    setInterval(() => {
+      let index: number = Math.floor(Math.random() * this.names.length);
+      // this.name = this.names[index];
+      // this.disabled = !this.disabled;
+    }, 2000)
+  }
+  setName(name): void {
+    this.name = name
+  }
 }
